3ef787276a8300c1275bd23378fbadcef9b98625
Aleks <gostuding>
2023-07-07 01:25:31 +0300

    set main.go in gophermart dir

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/gostuding/goMarket/internal/logger"
+	"github.com/gostuding/goMarket/internal/server"
+	"github.com/gostuding/goMarket/internal/storage"
+)
+
+func main() {
+	cfg := server.NewConfig()
+	logger, err := logger.NewLogger()
+	if err != nil {
+		log.Fatal(err)
+	}
+	strg, err := storage.NewPSQLStorage(cfg.DBConnect)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = server.RunServer(cfg, strg, logger)
+	if err != nil {
+		logger.Fatalln(err)
+	}
+}
